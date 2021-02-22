3dd461dc608533c75fae44383bd89a12cfe2794e
kiran santosh upase <kvsupase@gmail.com>
2021-02-23 00:31:54 +0530

    Mail parking ticket to user

--- SendEmail.py
+++ SendEmail.py
@@ -1,5 +1,6 @@
 import smtplib
 import tkinter
+from tkinter import font
 
 
 class sendParkingTicket(object):
@@ -17,9 +18,10 @@ class sendParkingTicket(object):
         self.messageWindow.config(bg="#FA8072")
         self.messageWindow['pady'] = 5
         label = tkinter.Label(self.messageWindow, text='Enter your Email address', bg='#FA8072')
+        label['font'] = tkinter.font.Font(size=20, family='Helvetica', weight='bold')
         label.pack()
 
-        self.mailAddress = tkinter.Entry(self.messageWindow)
+        self.mailAddress = tkinter.Entry(self.messageWindow, width=25)
         self.mailAddress.pack()
 
         sendButton = tkinter.Button(self.messageWindow, text='Send', bg='#FF7F50', activebackground='#FFA500',
@@ -42,26 +44,41 @@ class sendParkingTicket(object):
             body = self.str
             message = 'Subject : {}\n\n{}'.format(subject, body)
             ob.sendmail("ParkingLotSystem12@gmail.com", self.receiverEmail, message)
-            print("send successful..")
+            # print("send successful..")
             ob.quit()
+
             self.messageWindow.destroy()
 
             self.window = tkinter.Tk()
             self.window.title("Parking Lot")
             self.window.geometry("200x70-200-400")
             self.window.config(bg="#FA8072")
-            self.window['pady'] = 5
+            self.window['pady'] = 20
 
             label = tkinter.Label(self.window, text='Email Sent Successfully!', bg="#FA8072")
             label.pack()
 
-            okButton = tkinter.Button(self.window, text='OK', bg='#FF7F50', activebackground='#FFA500',
-                                      command=self.window.destroy)
-            okButton.pack(side='bottom', anchor='s')
-
             self.window.minsize(200, 70)
             self.window.maxsize(200, 70)
+
+            self.window.after(2500, self.window.destroy)
+
             self.window.mainloop()
 
-        except smtplib.SMTPRecipientsRefused:
-            print("error")
+        except smtplib.SMTPRecipientsRefused as e:
+            self.messageWindow.destroy()
+            self.warning = tkinter.Tk()
+            self.warning.title("Parking Lot")
+            self.warning.geometry("200x70-250-400")
+            self.warning.config(bg="#FA8072")
+            self.warning["pady"] = 15
+
+            label = tkinter.Label(self.warning, text="Please enter correct Email!", bg="#FA8072")
+            label.pack()
+
+            self.warning.minsize(200, 70)
+            self.warning.maxsize(200, 70)
+
+            self.warning.after(2500, self.warning.destroy)
+
+            self.warning.mainloop()
